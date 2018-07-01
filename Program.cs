e4f2eee6f4fb7926de958fa1d1e9eba2841a2958
Koji Nakagawa <lightupthesky0627@gmail.com>
2018-07-01 11:47:17 -0700

    add Facade design pattern

---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mortgage = new Mortgage();
+
+            var customer = new Customer("Ann McKinsey");
+            
+            var eligible = mortgage.IsEligible(customer, 125000);
+
+            Console.WriteLine("\n" + customer.Name + " has been " + (eligible ? "Approved" : "Rejected"));
+            Console.ReadKey();
+        }
+    }
+
+    // The 'Subsystem Class' class
+    class Bank 
+    {
+        public bool HasSufficientSavings(Customer c, int amount)
+        {
+            Console.WriteLine("Check bank for " + c.Name);
+            return true;
+        }
+    }
+
+    // The 'Subsystem Class' class
+    class Credit
+    {
+        public bool HasGoodCredit(Customer c)
+        {
+            Console.WriteLine("Check credit for " + c.Name);
+            return true;
+        }
+    }
+
+    // The 'Subsystem Class' class
+    class Loan
+    {
+        public bool HasNoBadLoans(Customer c)
+        {
+            Console.WriteLine("Check loans for " + c.Name);
+            return true;
+        }
+    }
+
+    class Customer
+    {
+        private string _name;
+
+        public Customer(string name)
+        {
+            _name = name;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+    }
+
+
+    //'Facade' class
+    class Mortgage
+    {
+        private Bank _bank = new Bank();
+        private Loan _loan = new Loan();
+        private Credit _credit = new Credit();
+
+        public bool IsEligible(Customer cust, int amount)
+        {
+            Console.WriteLine($"{cust.Name} applies for {amount} loand \n");
+            var eligible = true;
+
+            if (!_bank.HasSufficientSavings(cust, amount))
+            {
+                eligible = false;
+            }
+            else if (!_loan.HasNoBadLoans(cust))
+            {
+                eligible = false;
+            }
+            else if (!_credit.HasGoodCredit(cust))
+            {
+                eligible = false;
+            }
+
+            return eligible;
+        }
+    }
+}
